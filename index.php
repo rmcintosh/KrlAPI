1eb59641c1adaf2b15ec1f382e9282b340f10409
Pxlbin <willblew@gmail.com>
2017-01-06 19:36:55 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+////////////////////
+//Author: Will Blew
+//Year: 2016
+//For: Krl
+////////////////////
+require 'flight/Flight.php';
+require 'core/routes.php';
+require 'core/dbCore.php';
+Flight::start();
+?>
