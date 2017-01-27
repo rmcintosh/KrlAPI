b5968a579062cd04da50b57eef1de9538040ea9e
Pxlbin <willblew@gmail.com>
2017-01-27 19:46:00 +0000

    More things

--- core/links.php
+++ core/links.php
@@ -6,16 +6,21 @@ if ($conn != NULL){
     $sql = "SELECT url AS url FROM url_redirects WHERE short='" . $id . "' LIMIT 1";
     $sth = $conn->prepare($sql);
     $sth->execute();
-    $result = $sth->fetch();
+    $result = $sth->fetch(PDO::FETCH_ASSOC);
 
     $url = $result;
+    if ($url != false){
+    return $url;
+    } else {
+     $url = array("url" => "Link not found!");
+     return $url;
+    }
 
 }
 else 
 {
-    $url = array("url","NULL");
-}
+    $url = array("url" => "There was an error getting this url");
     return $url;
 }
-
+}
 ?>
